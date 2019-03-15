5fb60ff834caf62c0506e1bd8fcb1875f412226a
lule <lule2016enn@gmail.com>
2019-03-15 12:56:15 +0800

    add ng-zorro-antd library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/** 配置 docker env variable **/
 import { EnvService } from './_shared/service/global/env.service';
+
+/** 配置 angular i18n **/
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
 
 export function envInit(envService: EnvService) {
   return () => envService.initialize().subscribe();
 }
+registerLocaleData(zh);
+
 
 @NgModule({
   declarations: [
@@ -18,9 +28,13 @@ export function envInit(envService: EnvService) {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgZorroAntdModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [
+    { provide: NZ_I18N, useValue: zh_CN },
     { provide: APP_INITIALIZER, useFactory: envInit, deps: [EnvService], multi: true}
   ],
   bootstrap: [AppComponent]
